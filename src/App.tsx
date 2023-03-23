8a37bc75c633f0af9a6cac6095fec6cffc3ed706
Mylena <myholiveira6@gmail.com>
2023-03-23 10:51:17 -0300

    ajustes

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import Board from "./components/Board/Board";
 
 function App() {
   return (
-    <main className="App">
-      <div className="bg-[url('/imagens/background.svg')] mt-8 w-[926px] h-[581px] bg-cover m-auto">
+    <main className="App py-16">
+      <div className="bg-[url('/imagens/background.svg')] mt-8 w-[926px] h-[581px] bg-cover m-auto ">
         <h1 className="text-black font-bold	text-4xl text-center p-16 pt-1">
           Kanban
         </h1>
